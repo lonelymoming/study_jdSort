ea628b15e4f5fad815948f340dd49da693de7582
1786572929@qq.com <493016405@qq.com>
2016-12-01 13:50:22 +0800

    add for Tsing

---
+++ commodity_sort/QYTsingTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  QYTsingTestViewController.h
+//  commodity_sort
+//
+//  Created by MAC on 2016/12/1.
+//  Copyright © 2016年 LGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYTsingTestViewController : UIViewController
+
+@end
